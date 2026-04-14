1171fa3505bd9dff2423e5f1ff1c6c1ccc3c6565
John Q. Herman <john@raccoonlagoon.com>
2026-04-14 14:22:09 -0400

    write schema attribute for sticker slot 4

--- src/OpenGen.cs
+++ src/OpenGen.cs
@@ -102,6 +102,8 @@ public partial class OpenGen : BasePlugin
         {
             if (id == 0) continue;
             SetOrAddAttr.Invoke(attrs.Handle, $"sticker slot {slot} id",       UintAsFloat((uint)id));
+            if (slot == 4)
+                SetOrAddAttr.Invoke(attrs.Handle, $"sticker slot {slot} schema", 0f);
             SetOrAddAttr.Invoke(attrs.Handle, $"sticker slot {slot} offset x", x);
             SetOrAddAttr.Invoke(attrs.Handle, $"sticker slot {slot} offset y", y);
             SetOrAddAttr.Invoke(attrs.Handle, $"sticker slot {slot} wear",     stickerWear);
